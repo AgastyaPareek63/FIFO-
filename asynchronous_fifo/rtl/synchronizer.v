cfb333b9e3b77f4eae4c797f39552be6faf2ffe7
Bhargav7551 <bhargavbb0608@gmail.com>
2026-08-24 11:53:22 +0530

    Update synchronizer.v

--- asynchronous_fifo/rtl/synchronizer.v
+++ asynchronous_fifo/rtl/synchronizer.v
@@ -1,1 +1,41 @@
+`timescale 1ns/1ps
 
+module synchronizer #(
+    parameter WIDTH = 5,
+    parameter SYNC_STAGES = 2
+)(
+    input  wire             clk,
+    input  wire             rst,
+    input  wire [WIDTH-1:0] din,
+    output wire [WIDTH-1:0] dout
+);
+
+    // Synchronizer Registers
+  
+    reg [WIDTH-1:0] sync_ff [0:SYNC_STAGES-1];
+
+    integer i;
+
+    // Synchronizer Logic
+   
+    always @(posedge clk or posedge rst)
+    begin
+        if (rst)
+        begin
+            for(i=0; i<SYNC_STAGES; i=i+1)
+                sync_ff[i] <= 0;
+        end
+        else
+        begin
+            sync_ff[0] <= din;
+
+            for(i=1; i<SYNC_STAGES; i=i+1)
+                sync_ff[i] <= sync_ff[i-1];
+        end
+    end
+
+    // Output
+    
+    assign dout = sync_ff[SYNC_STAGES-1];
+
+endmodule
